a055be6cb594cf8d1de0f6462c3e24caad1cc6a1
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-03-02 00:25:28 +0530

    (feat): add block object

--- index.ts
+++ index.ts
@@ -2,12 +2,27 @@ import * as crypto from 'crypto'
 
 class Transaction {
   constructor (
-    public amount : number,
-    public payer : string, // public
-    public payee : string // private
+    public amount: number,
+    public payer: string, // public
+    public payee: string, // public
   ) {}
 
   toString() {
     return JSON.stringify(this);
+  }
+}
+
+class Block {
+  constructor (
+    public prevHash: string,
+    public transaction: Transaction,
+    public ts = Date.now()
+  ) {}
+
+  get hash() {
+    const str = JSON.stringify(this);
+    const hash = crypto.createHash('SHA256');
+    hash.update(str).end();
+    return hash.digest('hex');
   }
 }
